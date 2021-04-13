90c248f3b6e2a35d4978e412290f04926363516a
myshine112 <wonderpiano0313@gmail.com>
2021-04-13 16:08:28 -0500

    Update verification scripts to include UniswapV2Pair

--- verify.sh
+++ verify.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
-mkdir -p verified-sources/
-cp -R contracts/uniswapv2/ verified-sources/contracts/
+# To use this script, first run ./verify.sh
+# Then with the results:
+# 1. Go to sourcify.dev
+# 2. Add metadata.json from build/metadata/<ContractName>/metadata.json to the upload dialog
+# 3. Add all files in sources-flattened-dir to the dialog
+# 4. Set the contract address and upload
+
+ROOTDIR="$(pwd)/$(dirname $0)"
+SOURCES_DIR=$ROOTDIR/sources-flattened-dir
+
+mkdir -p "$SOURCES_DIR"
+cp -R contracts/uniswapv2/ $SOURCES_DIR/contracts/
 
 CONTRACTS=(
     "contracts/UniswapV2Router02.sol"
     "contracts/UniswapV2Factory.sol"
+    "contracts/UniswapV2Pair.sol"
 )
 
 for CONTRACT in ${CONTRACTS[@]}; do
     echo "$CONTRACT"
     CBASENAME=$(basename $CONTRACT)
     CBASE=${CBASENAME%.sol}
-    CONTRACT_DIR=deployments/metadata/"$CBASE"/
-    mkdir -p "deployments/metadata/$CBASE"
-    docker run -v "$(pwd)/verified-sources/:/sources" -w /sources ethereum/solc:0.6.12 \
+    CONTRACT_DIR=$ROOTDIR/build/metadata/$CBASE/
+    mkdir -p "$CONTRACT_DIR"
+    docker run -v "$ROOTDIR/$SOURCES_DIR/:/sources" -w /sources ethereum/solc:0.6.12 \
         --optimize --optimize-runs 5000 \
         --combined-json abi,bin,bin-runtime,compact-format,devdoc,hashes,interface,metadata "$CONTRACT" \
         | jq ".contracts | to_entries | map(select(.key | contains(\":$CBASE\"))) | first | .value " \
         | jq ".metadata | fromjson" > $CONTRACT_DIR/metadata.json
 done
 
-cp -R verified-sources/contracts/**/*.sol verified-sources/
-rm -fr verified-sources/contracts/
+cp -R $SOURCES_DIR/contracts/**/*.sol "$SOURCES_DIR"
+rm -fr $SOURCES_DIR/contracts/
